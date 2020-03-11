ea04240e2ea85e720696645d27c26467aa75f5a0
Catherina <catherine.pchelina@gmail.com>
2020-03-11 16:20:59 -0700

    Add send_msg method and test

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -2,9 +2,19 @@
 require "httparty"
 
 module SlackApiWrapper
-  BASE_URL = "https://slack.com/api/"
+  POST_URL = "https://slack.com/api/chat.postMessage"
   API_KEY = ENV["SLACK_TOKEN"]
 
-  # Code goes here
+  def self.send_msg(message, channel_name)
+    response = HTTParty.post(POST_URL, {
+      headers: { 'Content-Type' => 'application/x-www-form-urlencoded' },
+      body: {
+        token: API_KEY,
+        channel: channel_name,
+        text: message
+      }
+    })
 
+    return response.code == 200 && response.parsed_response["ok"]
+  end
 end
--- test/slack_api_wrapper_test.rb
+++ test/slack_api_wrapper_test.rb
@@ -1,6 +1,11 @@
 # slack_api_wrapper_test.rb
 require_relative 'test_helper'
 
-describe SlackApiWrapper do
-
+describe "SlackApi" do
+  it "can send a valid message" do 
+    VCR.use_cassette("slack-posts") do
+      response = SlackApiWrapper.send_msg("I can post messages!", "general")
+      expect(response).must_equal true
+    end
+  end
 end
